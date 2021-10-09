9cc0b5d66896783a7d7e343f160ace5e0f586f33
你好2007 <2501482523@qq.com>
2021-10-09 17:53:47 +0800

    完成一级菜单开发

--- src/App/index.ts
+++ src/App/index.ts
@@ -1,4 +1,5 @@
-import { Component } from 'nefbl'
+import { Component, reactive } from 'nefbl'
+import urlFormat from '../tool/urlFormat'
 
 import style from './index.scss'
 import template from './index.html'
@@ -10,5 +11,32 @@ import template from './index.html'
 })
 export default class {
 
+    selected: any
+
+    $setup() {
+        return {
+            selected: reactive([]),
+            changeSelected(event) {
+                let target = event.target
+
+                this.selected[target.getAttribute('key')] = target.getAttribute('val')
+            }
+        }
+    }
+
+    $mounted() {
+
+        // 地址栏信息
+        let selected = urlFormat(window.location.href)
+
+        // 默认路由
+        let defaultRouter = ["html"]
+
+        // 判断，如果没有，就用默认路由
+        for (let i = 0; i < defaultRouter.length; i++) {
+            this.selected[i] = selected.router[i] ? selected.router[i] : defaultRouter[i]
+        }
+
+    }
 
 }
