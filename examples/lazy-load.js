7648de8bb9b287e18593994366c05e320ae8f038
你好2007 <2501482523@qq.com>
2021-11-19 11:03:36 +0800

    修复径向渐变 例子未登记导致的错误

--- examples/lazy-load.js
+++ examples/lazy-load.js
@@ -1,4 +1,5 @@
 export default {
     drag: () => import('./drag.html'),
-    "linear-gradient": () => import('./linear-gradient.html')
+    "linear-gradient": () => import('./linear-gradient.html'),
+    "radial-gradient": () => import('./radial-gradient.html')
 }
